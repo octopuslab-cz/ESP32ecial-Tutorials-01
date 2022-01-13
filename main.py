1900822dcce36fd4e0818d027a20d10c31d719cd
Štěpán Sukdol <93820201+NigelfromBBL@users.noreply.github.com>
2022-01-13 14:56:27 +0100

    fixed typos

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ disp7 = disp7_init()
 
 
 LIGHT_THRESHOLD = 900
-DELAY = 1 #pouse time in seconds
-print("test") #testing the display function
-piezzo.play_melody(jingle1) #testing the piezzo melody
+DELAY = 1 # pouse time in seconds
+print("test") # testing the display function
+piezzo.play_melody(jingle1) # testing the piezzo melody
 
 
 while True: 
     light_level = photosensor.get_adc_aver()
     print(light_level)
-    disp7.show(light_level) #showing the light level number
+    disp7.show(light_level) # showing the light level number
     if light_level > LIGHT_THRESHOLD:
         piezzo.play_melody(jingle1) #alarm will start ringing when the ESP is exposed to light
     sleep(DELAY)
